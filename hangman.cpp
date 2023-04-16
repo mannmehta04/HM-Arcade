cb4bd49796f00cc1ccfb62b4c261b1e720b7e554
mannmehta04 <mannmehta04@gmail.com>
2023-04-16 13:14:31 +0530

    Updated Hangman
    Progress - 90% (Major Part Complete)
    - Modified "_" instead of "*"
    - Updated words limit from '30' to '50' for each word
    - Added Space between each "_" for better view
    - Added Instructions for the Gameplay
    - Added Facility for a Hint by pressing ' . ' for up to 3 times.
    - Shifted Stickman to slightly right side for better view
    - Added Failure Message

--- hangman.cpp
+++ hangman.cpp
@@ -1,74 +1,131 @@
-#include<graphics.h>
-#include<iostream>
-#include<conio.h>
-#include<cstring>
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <graphics.h>
+#include <conio.h>
 
 using namespace std;
 
 int main()
 {
-    initwindow(800, 800, "Hangman Game");
-    char word[20] = "programming";  // change to char array
-    char display[20] = {'*','*','*','*','*','*','*','*','*','*','\0'};
-    char guess;
-    int i, wrongGuesses = 0;
+    int gd = DETECT, gm;
+    initwindow(1200, 700, "Hangman");
 
-    // draw initial blank spaces
+    if (graphresult() != 0) {
+        cout << "Error initializing graphics." << endl;
+        exit(1);
+    }
+
+    char words[][50] = {"class", "object", "inheritance", "abstraction", "polymorphism", "encapsulation", "template", "exception", "constructor", "destructor", "virtualfunction", "operatoroverloading", "friendfunction"};
+    int numWords = 6;
+    srand(time(NULL));  // seed
+    int randomIndex = rand() % numWords;
+    char word[20];
+    strcpy(word, words[randomIndex]);
+    
+    char display[40];
+	for(int i = 0; i < strlen(word)*2-1; i+=2) {
+	    display[i] = '_';
+	    display[i+1] = ' ';
+	}
+	display[strlen(word)*2-1] = '\0';
+
+
+
+    char guess;
+    bool fail=0;
+    int i, wrongGuesses = 0, len = strlen(word), hintsLeft = 3;
+
+    settextstyle(10, HORIZ_DIR, 2);
+    outtextxy(50, 50, "Welcome to Hangman! Stickman is trying to remain hidden from it's enemies.");
+    outtextxy(50, 75, "Guess the words related to Concepts of C++ correctly to keep the stickman hidden.");
+    outtextxy(50, 100, "Press any key to guess a letter. You have 6 chances to guess incorrectly.");
+    outtextxy(50, 125, "On Incorrect guess, Stickman will be slowly visible. Guess the word before so!");
+    outtextxy(50, 150, "Good luck!");
+
     settextstyle(10, HORIZ_DIR, 3);
     outtextxy(300, 500, display);
+    
+    settextstyle(10, HORIZ_DIR, 2);
+    outtextxy(50, 200, "Press '.' for a hint.");
 
     while (1) {
         guess = getch();
 
-        // check if guessed letter is in word
-        for (i = 0; i < strlen(word); i++) {
-            if (word[i] == guess) {
-                display[i] = guess;
+		if (guess == '.' && hintsLeft > 0) {
+            int randIndex;
+            do {
+                randIndex = rand() % len;
+            } while (display[randIndex * 2] != '_');
+            
+            display[randIndex * 2] = word[randIndex];
+            settextstyle(10, HORIZ_DIR, 2);
+            hintsLeft--;
+            outtextxy(50, 230 + (3 - hintsLeft) * 25, "Hint used!");
+            delay(200);
+        }
+		else if (guess != '.') {
+	        bool found = false;
+	        for (i = 0; i < len; i++) {
+	            if (word[i] == guess) {
+	                display[i*2] = guess;
+	                found = true;
+	            }
+	        }
+	
+	        if (!found) {
+	            wrongGuesses++;
+	        }
+		}
+
+        switch (wrongGuesses) {
+		    case 1:
+		        circle(750, 250, 50);
+		        break;
+		    case 2:
+		        line(750, 300, 750, 500);
+		        break;
+		    case 3:
+		        line(750, 350, 700, 400);
+		        break;
+		    case 4:
+		        line(750, 350, 800, 400);
+		        break;
+		    case 5:
+		        line(750, 500, 700, 600);
+		        break;
+		    case 6:
+		        line(750, 500, 800, 600);
+		        fail = 1;
+		        settextstyle(10, HORIZ_DIR, 4);
+		        outtextxy(80, 620, "You Failed to Save the Stickman :(");
+		        delay(2000);
+		        exit(0);
+		        break;
+		}
+
+        int asterisks = 0;
+        for (i = 0; i < len; i++) {
+            if (display[i*2] == '_') {
+                asterisks++;
             }
         }
-
-        // increment wrongGuesses if guess is wrong
-        if (strstr(display, "*")) {
-            wrongGuesses++;
-        }
-
-        // display hangman image based on number of wrong guesses
-        switch (wrongGuesses) {
-            case 1:
-                circle(550, 250, 50);
-                break;
-            case 2:
-                line(550, 300, 550, 500);
-                break;
-            case 3:
-                line(550, 350, 500, 400);
-                break;
-            case 4:
-                line(550, 350, 600, 400);
-                break;
-            case 5:
-                line(550, 500, 500, 600);
-                break;
-            case 6:
-                line(550, 500, 600, 600);
-                break;
-        }
-
-        // display current guess status
         settextstyle(10, HORIZ_DIR, 3);
         outtextxy(300, 500, display);
-
-        // if all letters guessed, exit loop
-        if (!strstr(display, "*")) {
+        
+        if (asterisks == 0) {
             break;
         }
     }
-
-    // display message
-    settextstyle(10, HORIZ_DIR, 4);
-    outtextxy(250, 100, "Congratulations, you guessed the word!");
+    
+    if(fail==0){
+    	settextstyle(10, HORIZ_DIR, 4);
+    	outtextxy(80, 580, "Congratulations, you guessed the word safely!");
+		delay(2000);
+		exit(0);
+	}
     getch();
     closegraph();
     return 0;
 }
-
